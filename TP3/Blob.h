0b9d4d86813c14232af6f22da65b4e8095b4945b
bautistasch <62912239+bautistasch@users.noreply.github.com>
2021-03-19 15:06:49 -0300

    primeras clases

--- TP3/Blob.h
+++ TP3/Blob.h
@@ -3,11 +3,23 @@
 
 #include "Point.h"
 
+typedef enum { NEW_BORN, BABY_BLOB, GROWN_BLOB, GOOD_OLD_BOB } etaryGroupType;
+
+
+
 class Blob {
 	unsigned int vel;
 	unsigned int angle;
 	Point pos;
+	etaryGroupType age;
+	unsigned foodCount;
+	bool isAlive;
+	void move(Point&)
+	{
+
+	}
 
 };
+
 
 #endif
